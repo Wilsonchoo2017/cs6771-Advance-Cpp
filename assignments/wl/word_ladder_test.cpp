96ef8758c44de6ff0453c6aa8b9d25612d3b5018
Wilsonchoo2017 <z5157656@ad.unsw.edu.au>
2019-06-22 04:20:39 +1000

    Fixed Clang

--- assignments/wl/word_ladder_test.cpp
+++ assignments/wl/word_ladder_test.cpp
@@ -1,10 +1,14 @@
 /*
 
   == Explanation and rational of testing ==
+  My approach to testing is going from simple testing to more complex testing. This is so that in the
+  early stages of my development I can make sure that my program doesn't fail in simple edge cases
+  and leaving more time to debug potentially more complex bugs.
 
-  Explain and justify how you approached testing, the degree
-   to which you're certain you have covered all possibilities,
-   and why you think your tests are that thorough.
+  Correctness: I used the reference solution and use its results to make my test scenarios. Thus
+            Correctness shouldn't be an issue.
+  Coverage: My test covers: no Path/single path with little nodes, branch checks, Cycle checks,
+            Multi Path solution Check, and including a more complex test scenario. These tests are the usually the edge cases when dealing with graph nodes thus should be able to cover all edge cases.
 
 */
 
@@ -17,23 +21,30 @@
 #include "assignments/wl/word_ladder.h"
 #include "catch.h"
 
-// White Box Testing
-SCENARIO("Simple Test 1: simply runs the program that has the only 2 and 3 nodes solution.") {
+SCENARIO("Simple Test 1: simply runs the program that has the only 0, 2 and 3 nodes solution.") {
   GIVEN("A dictionary with specific words") {
     std::unordered_set<std::string> lexicon;
     lexicon.insert("can");
     lexicon.insert("cat");
-    WHEN("Find Ladder is called it should return 2 nodes") {
+    lexicon.insert("den");
+    WHEN("Find Ladder is called with solution") {
       auto solution = FindLadder("can", "cat", lexicon);
       ListOfString referenced_solution = {{"can", "cat"}};
-      REQUIRE(solution == referenced_solution);
+      THEN("it should return 2 nodes") { REQUIRE(solution == referenced_solution); }
     }
-    GIVEN("An additional node into the dictionary") {
-      lexicon.insert("con");
-      WHEN("Find Ladder is called it should return 3 nodes") {
-        auto solution = FindLadder("cat", "con", lexicon);
-        ListOfString referenced_solution = {{"cat", "can", "con"}};
-        REQUIRE(solution == referenced_solution);
+    WHEN("Find Ladder is called with no solution") {
+      auto solution = FindLadder("can", "den", lexicon);
+      THEN("it should return 2 nodes") { REQUIRE(solution.empty()); }
+    }
+
+    WHEN("Find Ladder is called") {
+      GIVEN("An additional node into the dictionary") {
+        lexicon.insert("con");
+        WHEN("Find Ladder is called ") {
+          auto solution = FindLadder("cat", "con", lexicon);
+          ListOfString referenced_solution = {{"cat", "can", "con"}};
+          THEN("it should return 3 nodes") { REQUIRE(solution == referenced_solution); }
+        }
       }
     }
   }
@@ -64,7 +75,7 @@ SCENARIO("Test 2: Cycle Check and Multi Path Solutions Check") {
 
 SCENARIO("Test 3: Correctness in populated dictionary to check whether it will ignore unrelated "
          "words") {
-  GIVEN("A populated Dictioanry") {
+  GIVEN("A populated Dictionary") {
     std::unordered_set<std::string> lexicon;
     std::vector<std::string> relevant_data(
         {"cat", "cot", "cog", "bog", "bat", "con", "can", "cam"});
@@ -77,6 +88,30 @@ SCENARIO("Test 3: Correctness in populated dictionary to check whether it will i
       std::sort(solution.begin(), solution.end());
       ListOfString referenced_solution = {{"cat", "can", "con"}, {"cat", "cot", "con"}};
       THEN("it should return 2 path with 3 nodes") { REQUIRE(solution == referenced_solution); }
+    }
+  }
+}
+
+SCENARIO("Test 4: Correctness in More Branches of Paths") {
+  GIVEN("A populated Dictionary") {
+    std::unordered_set<std::string> lexicon;
+    std::vector<std::string> data(
+        {
+          "work", "fork", "pork", "word", "worm", "worn", "form", "port",
+          "wood", "bort", "wert", "foam", "peak", "pert", "pirn", "pert", "pood", "boat", "peat",
+          "blat", "pian", "flam", "plod", "flam", "flay", "play", "plat", "plan", "plat", "plot",
+          "play"
+        });
+    lexicon.insert(data.begin(), data.end());
+      WHEN("Find Ladder is Called") {
+        auto solution = FindLadder("work", "play", lexicon);
+        std::sort(solution.begin(), solution.end());
+        ListOfString referenced_solution = {
+            {"work", "fork", "form", "foam", "flam", "flay", "play"},
+            {"work", "pork" ,"port", "pert", "peat", "plat", "play"},
+            {"work", "worm", "form", "foam", "flam", "flay", "play"}
+        };
+        THEN("it should return some path with 7 nodes") { REQUIRE(solution == referenced_solution);}
     }
   }
 }
