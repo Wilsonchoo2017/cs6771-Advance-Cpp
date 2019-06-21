e9fd4342198d4d592b66f5edfd56786d55f2668b
Wilsonchoo2017 <z5157656@ad.unsw.edu.au>
2019-06-22 03:39:15 +1000

    Fixed Clang

--- assignments/wl/word_ladder.h
+++ assignments/wl/word_ladder.h
@@ -1,27 +1,24 @@
 #ifndef ASSIGNMENTS_WL_WORD_LADDER_H_
 #define ASSIGNMENTS_WL_WORD_LADDER_H_
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <deque>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <queue>
-#include <string>
-#include <deque>
+#include <utility>
+#include <vector>
 
 using ListOfString = std::vector<std::vector<std::string>>;
 
-struct WordNode
-{
+struct WordNode {
   int hopped;
   std::string word;
-  std::vector<WordNode *> previous_word_nodes;
-  WordNode (int hopped, std::string word): hopped(hopped), word(std::move(word)) {};// constructor
+  std::vector<WordNode*> previous_word_nodes;
+  WordNode(int hopped, std::string word) : hopped(hopped), word(std::move(word)) {}  // constructor
 };
 
-ListOfString FindLadder(std::string const &, std::string const &,
-    std::unordered_set<std::string> const &);
-void GetAllLadder(ListOfString &, std::deque<std::string> &, WordNode*);
+ListOfString
+FindLadder(std::string const&, std::string const&, std::unordered_set<std::string> const&);
+void GetAllLadder(ListOfString&, std::deque<std::string>&, WordNode*);
 
 #endif  // ASSIGNMENTS_WL_WORD_LADDER_H_
